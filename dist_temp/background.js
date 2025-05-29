335cd75d765eb49a85511d7466d3020313f8728d
MrGeneralQ <mrgeneralq@gmail.com>
2025-05-29 23:35:55 +0200

    removed other files

--- dist_temp/background.js
+++
@@ -1,1 +0,0 @@
-chrome.runtime.onInstalled.addListener(e=>{e.reason===chrome.runtime.OnInstalledReason.INSTALL&&console.log("Extension installed for the first time."),console.log("Extension installed.")});
